75e3169c9e792c7556bc203588e05daa503ebd95
Corneliz <cornelizyoselin@gmail.com>
2018-02-19 22:09:15 -0400

    UtilitarioIndex.js

---
+++ js/UtilitarioIndex.js
@@ -0,0 +1,36 @@
+$(window).load(function () {
+    setTimeout(function () {
+        $(".loader").addClass("fadeOut");
+    }, 1000);
+    setTimeout(function () {
+        $(".loader").addClass("Ocultar");
+    }, 1500);
+    $('.aniview').AniView();
+    if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1) {
+    } else {
+        $("article.FondoImg").addClass("bg-attachment-fixed");
+    }
+
+});
+$(window).scroll(function () {
+    if ($("nav").offset().top > 56) {
+        $("nav").addClass("bg-white-light");
+        $(".button-previous").css("visibility", "visible");
+    } else {
+        $("nav").removeClass("bg-white-light");
+        $(".button-previous").css("visibility", "hidden");
+    }
+});
+$(document).ready(function () {
+    $('a[href^="#"]').click(function () {
+        var destino = $(this.hash);
+        if (destino.length == 0) {
+            destino = $('a[name="' + this.hash.substr(1) + '"]');
+        }
+        if (destino.length == 0) {
+            destino = $('html');
+        }
+        $('html, body').animate({ scrollTop: destino.offset().top }, 800);
+        return false;
+    });
+});
