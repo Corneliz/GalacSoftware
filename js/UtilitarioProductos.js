de264e00e9e5b6206a24b3ce48e7f2bea4970e56
Corneliz <cornelizyoselin@gmail.com>
2018-03-02 13:37:39 -0400

    Ajuste en document ready

--- js/UtilitarioProductos.js
+++ js/UtilitarioProductos.js
@@ -1,4 +1,24 @@
-$(window).ready(function () {
+$(window).scroll(function () {
+    if ($("nav").offset().top > 56) {
+        $("nav").addClass("bg-white-light");
+        $(".button-previous").css("visibility", "visible");
+    } else {
+        $("nav").removeClass("bg-white-light");
+        $(".button-previous").css("visibility", "hidden");
+    }
+});
+$(document).ready(function () {
+    $('a[href^="#Banner-Producto""]').click(function () {
+        var destino = $(this.hash);
+        if (destino.length == 0) {
+            destino = $('a[name="' + this.hash.substr(1) + '"]');
+        }
+        if (destino.length == 0) {
+            destino = $('html');
+        }
+        $('html, body').animate({ scrollTop: destino.offset().top }, 800);
+        return false;
+    });
     setTimeout(function () {
         $(".loader").addClass("fadeOut");
     }, 200);
@@ -10,28 +30,6 @@ $(window).ready(function () {
     } else {
         $("article.FondoImg").addClass("bg-attachment-fixed");
     }
-});
-$(window).scroll(function () {
-    if ($("nav").offset().top > 56) {
-        $("nav").addClass("bg-white-light");
-        $(".button-previous").css("visibility", "visible");
-    } else {
-        $("nav").removeClass("bg-white-light");
-        $(".button-previous").css("visibility", "hidden");
-    }
-});
-$(document).ready(function () {
-    $('a[href^="#Banner-Producto"]').click(function () {
-        var destino = $(this.hash);
-        if (destino.length == 0) {
-            destino = $('a[name="' + this.hash.substr(1) + '"]');
-        }
-        if (destino.length == 0) {
-            destino = $('html');
-        }
-        $('html, body').animate({ scrollTop: destino.offset().top }, 800);
-        return false;
-    });
 });
 $(".rotate").click(function () {
     var vIconoDesplegar = $(this).find("i");
